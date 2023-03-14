dba883e0c509e43856af8d28892be3e692fe2981
Patriotpl <patriotpl22@gmail.com>
2023-03-14 03:06:03 +0100

    parsing standings data first draft

--- main.py
+++ main.py
@@ -2,12 +2,47 @@ import irsdk
 import sched
 import time
 import socket
-from env.settings import CLIENT_ADDRESS
+from env.settings import CLIENT_ADDRESS, LAPS_TO_CALC_AVG
+from itertools import groupby
 
 # scheduler set to check for a runnig ir instance every 10 seconds
 # when instance is detected this changes to 30 times a second
 IDLE_REFRESH_RATE = 10
 IN_GAME_REFRESH_RATE = 1/30     #TODO: make the delays changable from the phone app
+
+# flag indicating if global data pools were initiated
+INITIATED = False
+# drivers laps data pool
+DRIVERS_AND_LAPS = []
+# drivers lap times data pool
+DRIVERS_AND_LAP_TIMES = []
+# drivers pit laps data pool
+DRIVERS_AND_PIT_LAPS = []
+# drivers and positions data pool
+DRIVERS_AND_POSITIONS = []
+# CarIdx of tracked driver
+TRACKED_DRIVER_ID = 0
+# CarIdx of player
+PLAYER_ID = 0
+
+def all_equal(iterable) -> bool:
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
+
+def calculate_avg_lap_time() -> float:
+    return 0.1
+
+def initiate(drivers: int, player_ID: float) -> None:
+    global DRIVERS_AND_LAP_TIMES
+    global DRIVERS_AND_LAPS
+    global DRIVERS_AND_PIT_LAPS
+    global DRIVERS_AND_POSITIONS
+    global PLAYER_ID
+    DRIVERS_AND_LAP_TIMES = [[] * drivers]
+    DRIVERS_AND_LAPS = [[] * drivers]
+    DRIVERS_AND_PIT_LAPS = [[] * drivers]
+    DRIVERS_AND_POSITIONS = [[] * drivers]
+    PLAYER_ID = player_ID
 
 """
 This function parses the data from irsdk and returns it as a string.
@@ -15,8 +50,21 @@ For now it also calculates all the additional data like when given competitor wi
 This will change when I learn some more about kotlin to lessen the computer load.
 """
 def select_data(ir: irsdk.IRSDK) -> str:
-    # data to send
+    snapshot = ir
 
+    if not INITIATED:
+        initiate(len(ir["DriverInfo"]["Drivers"]), ir["PlayerCarIdx"]) # type: ignore
+    
+    global DRIVERS_AND_LAP_TIMES
+    global DRIVERS_AND_PIT_LAPS
+    global LAPS_TO_CALC_AVG
+    global DRIVERS_AND_LAPS
+    global DRIVERS_AND_POSITIONS
+    global TRACKED_DRIVER_ID
+    global PLAYER_ID
+
+    # data to send:
+    # -----------------------
     # standings:
     # -----------------------
     # notes:
@@ -27,7 +75,7 @@ def select_data(ir: irsdk.IRSDK) -> str:
     # -----------------------
     # list of competitors
     # their race position
-    # their on track position
+    # their on track position - don't know if sdk does that
     # their class
     # their cars
     # their last laptime
@@ -40,13 +88,70 @@ def select_data(ir: irsdk.IRSDK) -> str:
     # what laptimes would player need to catch up to given competitor 1 lap before race end (maybe make the amount of laps settable from the app?)
     # if they're chasing the player - in how many laps they're in striking distance and what laptimes the player needs to avoid being caught untill the end of session
     # number of mandatory pitstops player and competitors have yet to have cleared (and what needs to be done it these pitstops??)
+
+    drivers = []
+    for driver in snapshot["DriverInfo"]["Drivers"]: # type: ignore
+        pitting = False
+        pos_status = "ok"
+        if snapshot["CarIdxOnPitRoad"][driver["CarIdx"]]:
+            pos_status = "pit_road"
+        elif snapshot["CarIdxInGarage"][driver["CarIdx"]]:  #TODO: check if this means that the car is being serviced, or retired
+            pos_status = "garage"
+            if snapshot["CarIdxLap"][driver["CarIdx"]] not in DRIVERS_AND_PIT_LAPS[driver["CarIdx"]]:
+                DRIVERS_AND_PIT_LAPS[driver["CarIdx"]].append(snapshot["CarIdxLap"][driver["CarIdx"]])
+                pitting = True
+        drivers.append({
+            "number": driver["CarNumber"],
+            "name": driver["UserName"],
+            "pos_status": pos_status,
+        })
+        drivers[-1]["car"] = "same" if driver["CarIdx"] != 0 and driver["CarID"] == drivers[0]["CarID"] else driver["CarScreenName"][:3] # type: ignore
+
+        # to do only on first loop or when refresh manually requested
+        if snapshot["WeekendInfo"]["WeekendOptions"]["IsFixedSetup"] != 0: # type: ignore
+            drivers[-1]["compound"] = snapshot["CarIdxTireCompound"][driver["CarIdx"]]
+            if not all_equal(d["CarClassID"] for d in snapshot["Drivers"]):
+                for driver in snapshot["DriverInfo"]["Drivers"]: # type: ignore
+                    driver["class"] = driver["CarClassID"]
+
+        # to do when driver's position changed
+        if snapshot[snapshot["CarIdxPosition"][driver["CarIdx"]]] != DRIVERS_AND_POSITIONS[snapshot["CarIdxPosition"][driver["CarIdx"]]]:
+            drivers[-1]["position"] = snapshot["CarIdxPosition"][driver["CarIdx"]]
+        
+        # to do when driver's pitting
+        if pitting:
+            drivers[-1]["pit_laps"] = DRIVERS_AND_PIT_LAPS[driver["CarIdx"]]
+            drivers[-1]["pits_count"] = len(DRIVERS_AND_PIT_LAPS[driver["CarIdx"]])
+
+        # to do when tracked driver or player completed a lap
+        if snapshot["CarIdxLap"][driver["CarIdx"]] != DRIVERS_AND_LAPS[driver["CarIdx"]] and driver["CarIdx"] in [PLAYER_ID, TRACKED_DRIVER_ID]:
+            avg_lap_time = round(sum(DRIVERS_AND_LAP_TIMES[driver["CarIdx"]]) / len(DRIVERS_AND_LAP_TIMES[driver["CarIdx"]]), 3)
+            drivers[-1]["avg_lap_time"] = avg_lap_time
+            drivers[-1]["tire_age"] = DRIVERS_AND_LAPS[driver["CarIdx"]] - DRIVERS_AND_PIT_LAPS[driver["CarIdx"]] if len(DRIVERS_AND_PIT_LAPS[driver["CarIdx"]]) > 0 else DRIVERS_AND_LAPS[driver["CarIdx"]]
+            drivers[-1]["last_lap_time"] = snapshot["CarIdxLastLapTime"][driver["CarIdx"]]
+            laps_to_strike = -1
+            if avg_lap_time > DRIVERS_AND_LAP_TIMES[PLAYER_ID]:
+                #laps_to_strike =   # maybe do this by summing up target drivers laptimes (all of them) and comparing to players?
+                pass 
+
+        # to do when any other driver completed a lap
+        elif snapshot["CarIdxLap"][driver["CarIdx"]] != DRIVERS_AND_LAPS[driver["CarIdx"]] and driver["CarIdx"] not in [PLAYER_ID, TRACKED_DRIVER_ID]:
+            # store data for later calculations
+            DRIVERS_AND_LAP_TIMES[driver["CarIdx"]].append(snapshot["CarIdxLastLapTime"][driver["CarIdx"]])
+            if len(DRIVERS_AND_LAP_TIMES[driver["CarIdx"]]) > LAPS_TO_CALC_AVG:
+                DRIVERS_AND_LAP_TIMES[driver["CarIdx"]] = DRIVERS_AND_LAP_TIMES[driver["CarIdx"]][1:]
+
+            # prep data for sending
+            drivers[-1]["avg_lap_time"] = round(sum(DRIVERS_AND_LAP_TIMES[driver["CarIdx"]]) / len(DRIVERS_AND_LAP_TIMES[driver["CarIdx"]]), 3)
+            drivers[-1]["tire_age"] = DRIVERS_AND_LAPS[driver["CarIdx"]] - DRIVERS_AND_PIT_LAPS[driver["CarIdx"]] if len(DRIVERS_AND_PIT_LAPS[driver["CarIdx"]]) > 0 else DRIVERS_AND_LAPS[driver["CarIdx"]]
+            drivers[-1]["last_lap_time"] = snapshot["CarIdxLastLapTime"][driver["CarIdx"]]
     
     # session
     # -----------------------
     # notes:
     # some of the settings appearing here might change how much data is being sent for other views
     # most of the data here can be sent once at the beginning of the session and then stored (or tracked in case of time of day) on clientside
-    # tho idk about weather data, if ir has random weather this might need to be tracked here and sent on change
+    # tho idk about weather data, if snapshot has random weather this might need to be tracked here and sent on change
     # refresh data button on the client's side will be helpful anyway
     # -----------------------
     # track name
@@ -86,6 +191,7 @@ def select_data(ir: irsdk.IRSDK) -> str:
     # overall session best laptime
     # players current laptime (can be tracked clientside i think?; this might actually be useless)
     # players all time best and optimal laptime?
+
     return str(ir["TrackName"])
 
 """
@@ -94,28 +200,25 @@ It simply tries to grab the data from api and preps the next scheduller event.
 If trying to get the data fails the delay is set to 10 seconds.
 Else delay is set to 1/30 of a second.
 """
-def main_loop(scheduler: sched.scheduler, UDPServerSocket: socket.socket) -> None:
+def main_loop(scheduler: sched.scheduler, UDPServerSocket: socket.socket, ir: irsdk.IRSDK) -> None:
     msg = str.encode("")
 
-    ir = irsdk.IRSDK()      # should this be in the main loop, or main function below?
-    ir.startup()
+    if not ir.is_connected:   
+        #UDPServerSocket.sendto(str.encode("iR 404"), CLIENT_ADDRESS)    #blocked for testing without connection to client
+        scheduler.enter(IDLE_REFRESH_RATE, 1, main_loop, (scheduler, UDPServerSocket, ir))
+        return
 
-    try:
-        ir["TrackName"]         # this is what throws an error when the game client's not running
-        scheduler.enter(IN_GAME_REFRESH_RATE, 1, main_loop, (scheduler, UDPServerSocket))
-        msg = str.encode(select_data(ir))
-    except AttributeError:
-        scheduler.enter(IDLE_REFRESH_RATE, 1, main_loop, (scheduler, UDPServerSocket))
-        msg = str.encode("iR 404")
-    finally:
-        UDPServerSocket.sendto(msg, CLIENT_ADDRESS)
-
-    
+    msg = str.encode(select_data(ir))
+    UDPServerSocket.sendto(msg, CLIENT_ADDRESS)
+    scheduler.enter(IN_GAME_REFRESH_RATE, 1, main_loop, (scheduler, UDPServerSocket, ir))    
 
 if __name__ == "__main__":
+    ir = irsdk.IRSDK()
+    ir.startup()
+
     UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     UDPServerSocket.bind(("localhost", 20002))
 
     scheduler = sched.scheduler(time.time, time.sleep)
-    scheduler.enter(0, 1, main_loop, (scheduler, UDPServerSocket))
+    scheduler.enter(0, 1, main_loop, (scheduler, UDPServerSocket, ir))
     scheduler.run()
--- settings.template.py
+++ settings.template.py
@@ -2,5 +2,9 @@
 # create "env" folder inside the app's main directory
 # move this file to the "env" folder
 
+# connection settings
 CLIENT_ADDRESS = ("ip", 0)  # ip addresses should be of str type; replace 0 with correct port
 SERVER_ADDRESS = ("ip", 0)  # ip addresses should be of str type; replace 0 with correct port
+
+# persistent variables
+LAPS_TO_CALC_AVG = 5
